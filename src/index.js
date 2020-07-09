f7dfd1decefaebcb07e6e9133cb92aa07000dc1b
Jorge <jorgediazok@gmail.com>
2020-07-09 18:22:28 -0300

    creating routes

--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ const express = require('express');
 const app = express();
 require('./database');
 
+//Importando las rutas para usar
+app.use(require('./routes/index'));
+
 app.listen(3000);
 console.log('Server on Port', 3000);
